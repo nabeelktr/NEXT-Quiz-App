7fda79d86ba58bcc9e9d552bd0d7dc1dcea1e581
nabeelktr <nabee446@gmail.com>
2024-02-03 17:20:47 +0530

    user design finished

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from "tailwindcss";
+import withMT from "@material-tailwind/react/utils/withMT";
 
-const config: Config = {
+
+module.exports = withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,5 +17,7 @@ const config: Config = {
     },
   },
   plugins: [],
-};
-export default config;
+});
+
+
+
